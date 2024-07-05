f06222b85ec86891b3f0d05fcd2265db1a554e41
Thomas Hingray <thomashingray@hotmail.fr>
2024-07-05 10:14:16 +0200

    Création de la structure

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <header>
+            Bye
+        </header>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <header>
+            <h1>Thomas Hingray, <span>développeur</span> web et mobile</h1>
+        </header>
+    )
+}
+
+export default Header
